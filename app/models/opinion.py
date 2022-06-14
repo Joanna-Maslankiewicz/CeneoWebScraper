55b09fd3e57f090790cce1050490e7a0ce6b9330
Joanna-Maslankiewicz <s222964@student.uek.krakow.pl>
2022-06-14 16:31:52 +0200

    2022-06-14 JM

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -14,7 +14,6 @@ class Opinion():
         self.pros = pros
         self.cons = cons
         self.opinion_id = opinion_id
-        return self
     
     def extract_opinion(self, opinion):
         for key, value in selectors.items():
@@ -23,23 +22,10 @@ class Opinion():
         return self
 
     def __str__(self) -> str:
-        return f"{self.author}, {self.recommendation}, {self.stars}, {self.content}, {self.useful}, {self.useless}, {self.publish_date}, {self.purchase_date}, {self.pros}, {self.cons}, {self.opinion_id}"
+        return f"opinion_id: {self.opinion_id}<br>" + "<br>".join(f"{key}: {str(getattr(self, key))}" for key in selectors.keys())
 
     def __repr__(self) -> str:
-        return f"{self.author}, {self.recommendation}, {self.stars}, {self.content}, {self.useful}, {self.useless}, {self.publish_date}, {self.purchase_date}, {self.pros}, {self.cons}, {self.opinion_id}"
+        return f"Opinion(opinion_id={self.opinion_id}, " + ", ".join(f"{key}={str(getattr(self, key))}" for key in selectors.keys()) + ")"
 
     def to_dict(self) -> dict:
-        opinion ={
-            "author" : self.author,
-            "recommendation" : self.recommendation,
-            "stars" : self.stars, 
-            "content" : self.content,
-            "useful" : self.useful,
-            "useless" : self.useless,
-            "publish_date" : self.publish_date,
-            "purchase_date" : self.purchase_date,
-            "pros" : self.pros,
-            "cons" : self.cons,
-            "opinion_id" : self.opinion_id
-        }
-        return opinion
+        return {"opinion_id": self.opinion_id} | {key: getattr(self, key) for key in selectors.keys()}
